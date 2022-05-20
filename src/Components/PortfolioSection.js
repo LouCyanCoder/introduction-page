f660780269f838d4542bb53cb1ee08b553321bab
Lucian Nguyen <lucian.n@seznam.cz>
2022-05-20 13:17:48 +0200

    added to portfolio and changed responsivity

--- src/Components/PortfolioSection.js
+++ src/Components/PortfolioSection.js
@@ -7,11 +7,11 @@ const PortfolioSection = () => {
 
 			<div className="grid-container">
 				<article className="card text-center col">
-					{/* <img
-						src={require("../img/placeholder.png")}
-						alt="project-image"
+					<img
+						src={require("../img/projects/generic-hipster-coffee.png")}
+						alt="project"
 						className="portfolio__img"
-					/> */}
+					/>
 					<div className="card-body">
 						<h5 className="card-title">Generic Hipster café</h5>
 						<p className="card-text">
@@ -21,15 +21,18 @@ const PortfolioSection = () => {
 						<p>
 							<strong>Tech stack:</strong> HTML, CSS, Sass, Bootstrap
 						</p>
-						<a href="#" className="btn btn-primary">
-							Visit site
+						<a
+							href="https://github.com/LouCyanCoder/generic-hipster-coffee"
+							className="btn btn-warning"
+						>
+							Github
 						</a>
 					</div>
 				</article>
 				<article className="card text-center col">
 					<img
 						src={require("../img/projects/laravel-jobs.png")}
-						alt="project-image"
+						alt="project"
 						className="portfolio__img"
 					/>
 					<div className="card-body">
@@ -56,22 +59,28 @@ const PortfolioSection = () => {
 					</div>
 				</article>
 				<article className="card text-center col">
-					{/* <img
-						src={require("../img/placeholder.png")}
-						alt="project-image"
+					<img
+						src={require("../img/projects/who-cares.png")}
+						alt="project"
 						className="portfolio__img"
-					/> */}
+					/>
 					<div className="card-body">
-						<h5 className="card-title">Generic Hipster café</h5>
+						<h5 className="card-title">Who cares?</h5>
 						<p className="card-text">
-							Website for a coffee shop. Student Project for the Coding Bootcamp
-							Praha. Coded based on provided psd design.
+							A portal for philantropists, enabling people of goodwill to post
+							their offers for help. Using leaflet for map component.
 						</p>
 						<p>
-							<strong>Tech stack:</strong> HTML, CSS, Sass, Bootstrap
+							<strong>Tech stack:</strong> Laravel, MySQL, React
 						</p>
-						<a href="#" className="btn btn-primary">
+						<a href="https://whocares.codeboot.cz/" className="btn btn-primary">
 							Visit site
+						</a>
+						<a
+							href="https://github.com/LouCyanCoder/laravel-final-project"
+							className="btn btn-warning m-2"
+						>
+							Github
 						</a>
 					</div>
 				</article>
