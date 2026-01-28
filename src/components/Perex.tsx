3bb2ea0cd6b49e82f2b98bfeafebda5d7b320d50
LouCyanCoder <lucian.n@seznam.cz>
2026-01-28 17:44:06 +0100

    redesign branches merged

--- src/components/Perex.tsx
+++ src/components/Perex.tsx
@@ -54,7 +54,7 @@ export default function NextSection() {
                 scrub: true,
                 pin: false,
                 invalidateOnRefresh: true,
-                markers: true
+                // markers: true
             },
         })
 
